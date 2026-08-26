3c60148073095d0a5b86fa5ee01d68be85f3d16b
Aayush049 <ayush.sri0108@gmail.com>
2026-08-27 01:33:14 +0530

    Add welcome screen

---
+++ app/src/main/java/com/androidlearning/brewly/screens/welcomescreen/WelcomeScreen.kt
@@ -0,0 +1,85 @@
+package com.androidlearning.brewly.screens.welcomescreen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.androidlearning.brewly.R
+import com.androidlearning.brewly.ui.theme.LightBrown
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun WelcomeScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Black)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.homescreen),
+            contentDescription = "Welcome Image"
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(vertical = 70.dp, horizontal = 24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            Text(text = "Fall in Love with Coffee with Blissful Delight!",
+                color = Color.White,
+                fontSize = 26.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(text = "Welcome to our cozy coffee corner",
+                color = Color.LightGray,
+                textAlign = TextAlign.Center,
+                fontSize = 16.sp
+            )
+
+            Spacer(modifier = Modifier.height(50.dp))
+
+            Button(
+                onClick = { /* Handle button click */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = LightBrown, //Directly Assessable
+                    contentColor = Color.White
+                ),
+                shape = RoundedCornerShape(12.dp)
+            ) {
+                Text(
+                    text = "Get Started",
+                    fontSize = 18.sp
+                )
+            }
+        }
+    }
+}
